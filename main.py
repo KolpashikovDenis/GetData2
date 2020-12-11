ce2b4eb10d6c7dc2075737b14a1e2bd213cc481b
KolpaschikovDenis <kolpashikov.denis@gmail.com>
2020-12-11 19:19:52 +0400

    Первонах

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-configfilename = os.path.abspath(os.path.dirname(sys.argv[0])) + '\properties.ini'
-config = configparser.ConfigParser()
-
-config.read(configfilename)
-hostname = config['DEFAULT']['hostname']
-login = config['DEFAULT']['login']
-password = config['DEFAULT']['password']
-str_date = config['DEFAULT']['date']
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    configfilename = os.path.abspath(os.path.dirname(sys.argv[0])) + '\properties.ini'
+    config = configparser.ConfigParser()
+
+    config.read(configfilename)
+    hostname = config['DEFAULT']['hostname']
+    login = config['DEFAULT']['login']
+    password = config['DEFAULT']['password']
+    str_date = config['DEFAULT']['date']
+
     path = os.path.abspath(os.path.dirname(sys.argv[0])) + '\\' + config['DEFAULT']['folder']
     if not os.path.exists(path):
         os.mkdir(path)
